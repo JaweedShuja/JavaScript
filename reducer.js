ee4089bcd02fb20b47874b15159e192cad691631
JaweedShuja <56365178+JaweedShuja@users.noreply.github.com>
2020-05-03 18:27:10 +0500

    Update reducer.js

--- reducer.js
+++ reducer.js
@@ -5,21 +5,21 @@ let INCEASE_COUNTER = 15
 
 const reducer = (state, action) => {
   //state === {red:number, green:number, blue:number}
-  //action === {colorToChnage:'red' || 'green' || 'blue', amount:15 || -15}
+  //action === {type:'change_red' || 'change_green' || 'change_blue', payload:15 || -15}
 
-  switch(action.colorToChange){
-    case 'red' :
-       return state.red + action.amount > 255 || state.red + action.amount < 0
+  switch(action.type){
+    case 'change_red' :
+       return state.red + action.payload > 255 || state.red + action.payload < 0
        ? state
-       : {...state, red: state.red + action.amount}
-    case 'green' :
-        return state.green + action.amount > 255 || state.green + action.amount < 0
+       : {...state, red: state.red + action.payload}
+    case 'change_green' :
+        return state.green + action.payload > 255 || state.green + action.payload < 0
         ? state
-        : {...state, green: state.green + action.amount}
-    case 'blue' :
-        return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        : {...state, green: state.green + action.payload}
+    case 'change_blue' :
+        return state.blue + action.payload > 255 || state.blue + action.payload < 0
         ? state
-        : {...state, blue: state.blue + action.amount}
+        : {...state, blue: state.blue + action.payload}
     default :
       return state;
 
@@ -33,16 +33,16 @@ const App = () => {
 
   return <View>
       <CounterComponent 
-      onIncrease={() => dispatch({colorToChange:'red', amount:INCEASE_COUNTER})}
-      onDecrease={() => dispatch({colorToChange:'red', amount:-1*INCEASE_COUNTER})}
+      onIncrease={() => dispatch({type:'change_red', payload:INCEASE_COUNTER})}
+      onDecrease={() => dispatch({type:'change_red', payload:-1*INCEASE_COUNTER})}
       color="Red"/>
       <CounterComponent 
-      onIncrease={() => dispatch({colorToChange:'green', amount:INCEASE_COUNTER})}
-      onDecrease={() => dispatch({colorToChange:'green', amount:-1*INCEASE_COUNTER})}
+      onIncrease={() => dispatch({type:'change_green', payload:INCEASE_COUNTER})}
+      onDecrease={() => dispatch({type:'change_green', payload:-1*INCEASE_COUNTER})}
       color="Green"/>
       <CounterComponent 
-      onIncrease={() => dispatch({colorToChange:'blue', amount:INCEASE_COUNTER})}
-      onDecrease={() => dispatch({colorToChange:'blue', amount:-1*INCEASE_COUNTER})}
+      onIncrease={() => dispatch({type:'change_blue', payload:INCEASE_COUNTER})}
+      onDecrease={() => dispatch({type:'change_blue', payload:-1*INCEASE_COUNTER})}
       color="Blue"/>
 
       <View style={{height:150, width:150, backgroundColor:`rgb(${red},${green},${blue})`}}>
